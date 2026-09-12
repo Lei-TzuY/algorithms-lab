d73e8c56258e414499fc5376cda8399c7af340e2
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-13 01:41:49 +0800

    test: register Elias-Fano recovery cases

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -13,6 +13,7 @@
 #include "test_reed_solomon_cases.hpp"
 #include "test_vertex_connectivity_cases.hpp"
 #include "test_radix_heap_cases.hpp"
+#include "test_elias_fano_cases.hpp"
 #include "test_smith_normal_form_cases.hpp"
 #include "test_modular_linear_system_cases.hpp"
 
