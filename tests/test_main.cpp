a74e8d4c4d6e9689fba23de43e1151a779f6e017
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-13 03:01:23 +0800

    test: register fractional cascading recovery

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -18,6 +18,7 @@
 #include "test_modular_linear_system_cases.hpp"
 #include "test_weighted_matroid_intersection_cases.hpp"
 #include "test_k_shortest_paths_cases.hpp"
+#include "test_fractional_cascading_cases.hpp"
 
 int main() {
   std::size_t passed = 0;
