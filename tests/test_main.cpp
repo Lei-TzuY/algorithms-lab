fb80ee31e493730192fe958e6f26d01dbbf34f0b
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-14 04:37:06 +0800

    test: register PLU recovery cases

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -41,6 +41,7 @@
 #include "test_weighted_set_cover_cases.hpp"
 #include "test_conjugate_gradient_cases.hpp"
 #include "test_cholesky_cases.hpp"
+#include "test_lu_factorization_cases.hpp"
 #include "test_householder_qr_cases.hpp"
 #include "test_bidiagonalization_cases.hpp"
 #include "test_minimum_enclosing_circle_cases.hpp"
