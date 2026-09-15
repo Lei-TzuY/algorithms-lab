57189e0d25867bd0d16c74213af6bbcec1fd085d
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-16 03:07:23 +0800

    test: register Kruskal reconstruction cases

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -88,6 +88,7 @@
 #include "test_pairing_heap_cases.hpp"
 #include "test_soft_heap_cases.hpp"
 #include "test_quotient_filter_cases.hpp"
+#include "test_kruskal_reconstruction_forest_cases.hpp"
 
 int main() {
   std::size_t passed = 0;
