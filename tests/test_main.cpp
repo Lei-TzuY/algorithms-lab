cbe6bee297cdbcfadda06d1cc8c363b52865d9f4
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-15 22:52:13 +0800

    Scope recovery: directed Matrix-Tree arborescence counting (#312)
    * feat: add directed arborescence counting
    
    * test: verify directed arborescence counting
    
    * docs: record directed arborescence count proof boundary
    
    * test: register directed arborescence count cases

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,6 +3,7 @@
 #include "test_equal_degree_factorization_cases.hpp"
 #include "test_irreducible_factorization_cases.hpp"
 #include "test_spanning_tree_count_cases.hpp"
+#include "test_directed_arborescence_count_cases.hpp"
 #include "test_earley_parser_cases.hpp"
 #include "test_earley_parser_randomized_cases.hpp"
 #include "test_extension_field_cases.hpp"
