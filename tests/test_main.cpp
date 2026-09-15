34ce274fc00e18291ffe243f93c2797a39dd7a82
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-16 05:30:22 +0800

    test: register Myers bit-vector edit-distance cases

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -90,6 +90,7 @@
 #include "test_quotient_filter_cases.hpp"
 #include "test_disjoint_sparse_table_cases.hpp"
 #include "test_xor_linear_basis_cases.hpp"
+#include "test_myers_edit_distance_cases.hpp"
 
 int main() {
   std::size_t passed = 0;
