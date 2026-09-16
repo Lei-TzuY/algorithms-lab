099625d46495dea1968e20c631708f77b003f5c5
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-16 12:33:10 +0800

    test: register KLL quantile sketch cases

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -99,6 +99,7 @@
 #include "test_frequent_directions_cases.hpp"
 #include "test_viterbi_convolutional_cases.hpp"
 #include "test_bounded_half_plane_intersection_cases.hpp"
+#include "test_kll_quantiles_cases.hpp"
 
 int main() {
   std::size_t passed = 0;
