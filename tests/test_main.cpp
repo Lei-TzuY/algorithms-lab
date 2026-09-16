8261a0e6f44a2724ebcb200ae427f8abc5d67342
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-16 23:28:39 +0800

    test: register squared distance transform recovery

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -110,6 +110,7 @@
 #include "test_schroeppel_shamir_cases.hpp"
 #include "test_deterministic_selection_cases.hpp"
 #include "test_isotonic_regression_cases.hpp"
+#include "test_squared_distance_transform_cases.hpp"
 
 int main() {
   std::size_t passed = 0;
